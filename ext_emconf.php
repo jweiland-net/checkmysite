8cb57f3bc0a6dd3fd878894d92f74f89e300f8d0
Stefan Froemken <froemken@gmail.com>
2016-06-30 12:06:03 +0200

    Compatible with TYPO3 8.*

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.99.99',
         ),
         'conflicts' => array(
         ),
